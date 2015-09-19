63bb00cad0186a7d74ac0648e553c47b38c9d6dc
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-09-19 13:34:16 -0500

    modern tooling added

---
+++ javascripts/entry.js
@@ -0,0 +1,24 @@
+require.config({
+  baseURL: "./javascripts",
+  paths:{
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "firebase":"../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash"
+  },
+  shim: {
+    "bootstrap":["jquery"],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
+
+
+require(["jquery", "hbs", "bootstrap", "firebase"],
+  function($, Handlebars, bootstrap, firebase) {
+
+
+  }
+);
